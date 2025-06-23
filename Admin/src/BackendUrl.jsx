d44b11511b1eec32e159ab5615ac5ca26ea1bdfd
nashad04 <nashad04online@gmail.com>
2025-06-23 15:08:38 +0530

    done

--- Admin/src/BackendUrl.jsx
+++ Admin/src/BackendUrl.jsx
@@ -1,1 +1,2 @@
-export const BackendUrl = "http://127.0.0.1:8000";
+// export const BackendUrl = "http://127.0.0.1:8000";
+export const BackendUrl = "http://13.233.254.191:8000";
--- Backend/travelFinalApi/settings.py
+++ Backend/travelFinalApi/settings.py
@@ -148,6 +148,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "http://13.233.254.191",
 ]
 
 
